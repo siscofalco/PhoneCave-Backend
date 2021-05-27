a3e1f9110653f20183d72c6f7b6c63de682b30ca
Sisco Falcó <siscofalcop@gmail.com>
2021-05-27 11:59:24 +0200

    added backend routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router  = express.Router();
+const Phone = require('../models/Phone.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  return res.status(200).json;
 });
+
+// GET Phone details
+
+router.get('/:id', req, res, next) => {
+  const { id } = req.params;
+  Phone.findOne({ _id: id })
+  .then(data => res.status(200).json(data))
+  .catch(error => res.status(500).json(error))
+}
 
 module.exports = router;
