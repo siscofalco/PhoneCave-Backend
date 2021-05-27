0120571df0f280e87ca2e653bac28d69554c112e
Sisco Falcó <siscofalcop@gmail.com>
2021-05-27 11:44:59 +0200

    added phones.json

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
 
 mongoose
   .connect('mongodb://localhost/phonecave-backend', {useNewUrlParser: true})
@@ -38,21 +36,13 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
